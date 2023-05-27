5e1273da29e8161698c59b5a572b6142ff3443d0
anick <anick@anicks-mbp>
2023-05-27 00:42:52 -0400

    practice

---
+++ src/Abstractconceptpractice/Base.java
@@ -0,0 +1,44 @@
+package Abstractconceptpractice;
+
+public class Base implements  Basecalculator,car {
+
+
+	@Override
+	public void add(int a, int b) {
+	int add = a+b;
+		
+	}
+
+	
+	
+
+	public static void main(String[] args) {
+	
+
+	}
+
+	
+
+	@Override
+	public void sub(int a, int b) {
+	int sub = a-b;
+		
+	}
+
+	@Override
+	public void start() {
+		
+		
+	}
+
+	@Override
+	public void stop() {
+		
+		
+	}
+
+	
+
+	
+
+}
